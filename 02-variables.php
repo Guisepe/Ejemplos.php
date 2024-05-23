50eb044f3984a4c849d2ed08afb95bd6f2f6c5d7
Alonso Caceres <alonsitodiegoc982@gmail.com>
2024-05-23 15:54:46 -0500

    02variables

---
+++ 02-variables.php
@@ -0,0 +1,13 @@
+<?php
+$a = 5;
+$b = 10;
+$suma = $a + $b;
+$resta = $a - $b;
+$producto = $a * $b;
+$division = $b / $a;
+
+echo "Suma: $suma\n";        
+echo "Resta: $resta\n";      
+echo "Producto: $producto\n"; 
+echo "División: $division\n"; 
+?>
